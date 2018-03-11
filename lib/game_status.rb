53b11e2ea78630f6bb46946afcb0a92213af9440
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-11 15:49:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,14 +3,19 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+#WON?
+def won?(board)
+end
+
+
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
-  [6,7,9],
+  [6,7,8],
   [0,3,6],
   [1,4,7],
-  [2,5,9],
-  [0,4,9],
+  [2,5,8],
+  [0,4,8],
   [2,4,7]
 ]
